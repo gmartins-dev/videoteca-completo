765dae86293cbc15a56d3c6a583e09245d7d1bad
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-05-14 17:07:38 -0300

    faltar concertar o link dos videos

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import { Container } from "./styles";
+import { Container } from './styles';
 
 export default function Footer() {
   return (
     <Container>
-      <strong>Study app developed with ❤ by @huri3l</strong>
+      <strong>App created just for studies propose.</strong>
     </Container>
   );
 }
